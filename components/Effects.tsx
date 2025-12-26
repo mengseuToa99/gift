79b0251caedba9217ff1332b0ddbc4b1ef1611da
mengseuTao99 <taimengseu7777@gmail.com>
2025-12-27 06:43:20 +0700

    change

--- components/Effects.tsx
+++ components/Effects.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { EffectComposer, Bloom, Vignette } from '@react-three/postprocessing';
 import { KernelSize, Resolution } from 'postprocessing';
+import { CONFIG } from '../constants';
 
 export const Effects: React.FC = () => {
+  const isMobile = CONFIG.isMobile;
+  
   return (
     <EffectComposer disableNormalPass>
       <Bloom 
-        intensity={1.5} 
+        intensity={isMobile ? 0.8 : 1.5} 
         luminanceThreshold={0.2} 
         luminanceSmoothing={0.9} 
-        kernelSize={KernelSize.LARGE} 
-        mipmapBlur 
+        kernelSize={isMobile ? KernelSize.SMALL : KernelSize.LARGE} 
+        mipmapBlur={!isMobile}
       />
-      <Vignette eskil={false} offset={0.1} darkness={0.5} />
+      {!isMobile && <Vignette eskil={false} offset={0.1} darkness={0.5} />}
     </EffectComposer>
   );
 };
